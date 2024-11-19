aa553fba6afc83dae39b157725886ebf2a8e71d1
Tomas <Angelt.rodriguezp@gmail.com>
2024-11-19 15:48:30 -0500

    se subio a produccion
    Cambios en el archivo de la conexión a la base de datos

--- Config/App/config.php
+++ Config/App/config.php
@@ -2,10 +2,10 @@
 
 
     const base_url="http://localhost/daviplata/";
-    const host = "localhost";
-    const user = "root";
-    const pass=  "";
-    const db = "sistema";
+    const host = "sql107.infinityfree.com";
+    const user = "if0_37744792";
+    const pass=  "7SEgeK9nlaNZ1 ";
+    const db = "if0_37744792_sistema";
     const charset = "charset=uf8";
 
 
